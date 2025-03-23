3d4677b8cc5bacbf45ac72a587b524939ee8b6f7
Geetham Gampa <geethamgampa0905@gmail.com>
2025-03-23 15:54:47 -0400

    Topics taught in the class

---
+++ Work_1.py
@@ -0,0 +1,141 @@
+# a = 15
+# b =6
+# c = a // b # Floor Division
+# print(c)
+
+# #Modulus
+# # 7 % 2
+# print( 7 % 2)
+
+
+# for i in range(0,11):
+#   print(i)
+  
+# name = 'pavan'
+# print(name[0:3])
+
+# contact = "1234568890"
+# print(contact[0:5])
+
+
+# i = 0
+# while i < 11:
+#   print(i)
+#   i += 1
+  
+  
+#   for i in range(1, 11):
+#    if i % 2 == 0:
+#     print(i, "Even")
+#   else:
+#     print(i, "odd")
+    
+    
+# a = 7
+# print(a)
+# print(a == 6)
+# print(a > 6)
+# print(a <= 6)
+
+
+# for x in 'pavan':
+#   print(x)
+  
+  
+# x = -10
+# print(abs(x))
+
+# print(pow(5,3))
+
+# print(5**3)
+
+# a = 'banan'
+# b = 'apple'
+# for x in a:
+#    x = n
+#    for y in b:
+#      y = a
+#      print(x,y)
+# ### a[0] = b
+#      y[0]
+     
+     
+# #pass
+
+# x = 21
+# if x >= 20:
+#   print('x is greater then 20')
+# else:
+#     pass
+
+
+# for x in range(3):
+#   print(x)
+  
+  
+# print(13 % 4)
+# # 2) 3 ** 3
+# # print(9 // 2)
+
+
+# ## equal
+# a == b
+# a != b
+# a < b
+# a <= b
+# a > b
+# a >= b
+
+
+# from pickle import FALSE
+# x = 10
+# if x > 10 and x == 10:
+#   print("True")
+# T T = T
+# F T = False
+# F F = False
+# T F = F
+
+
+# T T = T
+# F T = T
+# F F = False
+# T F = True
+
+
+# x = 10
+# print(x > 10)
+
+
+# print(2 % 2)
+
+
+# #indentation
+# if x >= 10:
+#  print("x is greater")
+ 
+ 
+# x = 9
+
+# if x >= 20:
+#   print("greater")
+# elif x == 20:
+#   print("equal")
+# else:
+#   print("small")
+
+
+
+# x = 20
+# # if x > 20:
+# #   print("greater")
+# # elif x == 20:
+# #   print("same")
+
+# print(x > 20)
+
+
+
+    
+    
+    
