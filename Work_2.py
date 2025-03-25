2d63d294e3fa09ddda3acda7253dc97f3da7ded1
GeethamGampa <geethamgampa0905@gmail.com>
2025-03-25 12:58:55 -0400

    Printing the given year is leap or not

--- Work_2.py
+++ Work_2.py
@@ -1,12 +1,11 @@
 #Check whether the given year is leap or not using functions
 
 def is_leap(year):
-    
-    # Write your logic here
+
     if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
-     return True
+     return true
     else:
-        return False
+        return false
     
 year = int(input("Enter the year: "))
 print(is_leap(year))
