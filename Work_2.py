5356d3d3c30da6c39246b2b30da7b9db6725b020
GeethamGampa <geethamgampa0905@gmail.com>
2025-03-25 12:51:12 -0400

    Update Work_2.py

--- Work_2.py
+++ Work_2.py
@@ -1,11 +1,10 @@
 #Check whether the given year is leap or not using functions
 
 def is_leap(year):
-    leap = True
     
     # Write your logic here
     if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
-     return leap
+     return True
     else:
         return False
     
